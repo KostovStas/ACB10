d11f2a45dcd1baa7ba93a10689fa1776fbfafaa1
gorobec <yevhenijvorobiei@gmail.com>
2016-01-12 13:42:31 +0200

    f

--- src/home_work/week_3_2/ReplaceAllNumbers_3_1.java
+++ src/home_work/week_3_2/ReplaceAllNumbers_3_1.java
@@ -1,8 +1,8 @@
 package home_work.week_3_2;
 
 
-// Матрицы генерировать случайными числами и размер матрицы вводит юзер.
-//3.1. Заменить все числа, которые делятся на 5 без остачи в двухмерной матрице на 8
+// пїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅ пїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅ.
+//3.1. пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅ, пїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅ 5 пїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅ пїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅ 8
 //Matrix to generate random numbers and the size of the matrix entered by user.
 //3.1. Replace all numbers, which are divided into 5 without astaci in a two-dimensional matrix for 8
 
@@ -24,18 +24,11 @@ public class ReplaceAllNumbers_3_1 {
                 if (matrix[i][j] % 5 == 0) {
 
                     matrix[i][j]=8;
-
-
-
                 }
-
             }
-            MultiArrayUtils.printMultiArray(matrix);
         }
-
-
+        MultiArrayUtils.printMultiArray(matrix);
     }
-
-    }
+}
 
 
